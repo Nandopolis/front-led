53d2261a5d6efb14e4bd5b658d9af8867358db5b
Hernando Calle Valencia <hernando_valencia@icloud.com>
2019-05-15 03:56:03 -0400

    websocket test

--- src/store.js
+++ src/store.js
@@ -5,10 +5,69 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    socket: {
+      isConnected: false,
+      message: '',
+      reconnectError: false,
+    },
+    goal_lap: 2,
+    play: false,
+    vel: { player_1: 0, player_2: 0, time: 0.0},
+    pos: { player_1: 0, player_2: 0, time: 0.0},
+    lap: { player_1: 0, player_2: 0, time: 0.0},
+    winner: { player: 0, time: 0.0},
   },
   mutations: {
-
+    SOCKET_ONOPEN (state, event)  {
+      Vue.prototype.$socket = event.currentTarget
+      state.socket.isConnected = true
+    },
+    SOCKET_ONCLOSE (state, event)  {
+      console.error(state, event)
+      state.socket.isConnected = false
+    },
+    SOCKET_ONERROR (state, event)  {
+      console.error(state, event)
+    },
+    // default handler called for all methods
+    SOCKET_ONMESSAGE (state, message)  {
+      state.socket.message = message
+      var timed_states = []
+      var time = null
+      message.forEach(data => {
+        var data_type = data.data_type
+        if (['vel', 'pos', 'lap'].indexOf(data_type) >= 0) {
+          state[data_type].player_1 = data[data_type + '_1']
+          state[data_type].player_2 = data[data_type + '_2']
+          timed_states.push(state[data_type])
+        }
+        if (data_type == 'win') {
+          state.winner.player = data.winner
+          timed_states.push(state.winner)
+        }
+        if (data_type == 'time') {
+          time = data.seconds
+        }
+        if (data_type == 'goal') {
+          state.goal_lap = data.goal_lap
+        }
+        if (data_type == 'play') {
+          state.play = data.status
+        }
+      })
+      if (time != null) {
+        timed_states.forEach(state => {
+          state.time = time
+        });
+      }
+    },
+    // mutations for reconnect methods
+    SOCKET_RECONNECT(state, count) {
+      console.info(state, count)
+    },
+    SOCKET_RECONNECT_ERROR(state) {
+      state.socket.reconnectError = true;
+    },
   },
   actions: {
 
---
+++ src/views/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <span>velocidad</span><br>
+    <span>jugador 1: {{ vel.player_1 }} </span><br>
+    <span>jugador 2: {{ vel.player_2 }}</span><br>
+    <span>time: {{ vel.time }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  computed: {
+    vel() {
+      return this.$store.state.vel
+    }
+  },
+}
+</script>
